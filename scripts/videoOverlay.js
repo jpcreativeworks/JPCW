08491d5d6803214d5961a92b27ceaaee4bbc450f
Jenn <96408342+jpcreativeworks@users.noreply.github.com>
2025-08-07 11:27:45 -0700

    started adding graphic design catalog iframe and styling.

--- scripts/videoOverlay.js
+++ scripts/videoOverlay.js
@@ -4,3 +4,9 @@ function playVideo(button) {
   overlay.style.display = "none";
   video.play();
 }
+
+function viewCatalog(button) {
+  console.log("Catalog button clicked");
+  const overlay = button.closest('.project-overlay');
+  overlay.style.display = "none";
+}
